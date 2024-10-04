020a3a9994b017f6e08808f94f9bfa9fa2ef5651
Dussan Freire <73646620+DussanFreire@users.noreply.github.com>
2024-10-04 07:21:09 -0400

    Create 125. Valid Palindrome.js

---
+++ 125. Valid Palindrome.js
@@ -0,0 +1,69 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+// Example 1:
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+// Example 2:
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+  
+// Example 3:
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+// Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+// Constraints:
+// 1 <= s.length <= 2 * 10^5
+// s consists only of printable ASCII characters.
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    let l = 0, r = s.length - 1;
+    while(l<=r){
+        const left = s[l].replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
+        if(!left) {
+            l++;
+            continue
+        }
+        const right = s[r].replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
+        if(!right) {
+            r--;
+            continue
+        }
+        if(left != right) {
+            return false;
+        }
+        else {
+            l++;
+            r--;
+        }
+    }
+
+    return true;
+};
+
+
+// OTHER ANSWER
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+// var isPalindrome = function (s) {
+//     s = s.replace(/[^0-9a-zA-Z]/g, '');
+    
+//     for (let i = 0; i < s.length / 2; i++) {
+//         if (s[i].toLowerCase() !== s[s.length - 1 - i].toLowerCase()) {
+//             return false;
+//         }
+//     }
+//     return true;
+// };
