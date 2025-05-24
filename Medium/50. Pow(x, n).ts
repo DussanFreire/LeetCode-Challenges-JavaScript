9ab635e55758721638f49c1af87c4528bd13428a
Dussan Freire <dussanfreire@gmail.com>
2025-05-24 19:07:37 -0400

    Create 50. Pow(x, n).ts

---
+++ Medium/50. Pow(x, n).ts
@@ -0,0 +1,36 @@
+// Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+// Example 1:
+// Input: x = 2.00000, n = 10
+// Output: 1024.00000
+
+// Example 2:
+// Input: x = 2.10000, n = 3
+// Output: 9.26100
+
+// Example 3:
+// Input: x = 2.00000, n = -2
+// Output: 0.25000
+// Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
+
+function myPow(x: number, n: number): number {
+  if (n === 0) return 1;
+
+  let N = n;
+  if (N < 0) {
+    x = 1 / x;
+    N = -N;
+  }
+
+  let result = 1;
+  while (N > 0) {
+    if (N % 2 === 1) {
+      result *= x;
+    }
+    x *= x;
+    N = Math.floor(N / 2);
+  }
+
+  return result;
+}
