0184290155fff968457b54f98af0d69838be0e8c
Dussan Freire <dussanfreire@gmail.com>
2025-06-06 23:25:42 -0400

    Create 47. Permutations II.ts

---
+++ Medium/47. Permutations II.ts
@@ -0,0 +1,45 @@
+// Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+// Example 1:
+// Input: nums = [1,1,2]
+// Output:
+// [[1,1,2],
+//  [1,2,1],
+//  [2,1,1]]
+
+// Example 2:
+// Input: nums = [1,2,3]
+// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+function permuteUnique(nums: number[]): number[][] {
+  const result: number[][] = [];
+  const used: boolean[] = Array(nums.length).fill(false);
+
+  nums.sort((a, b) => a - b); // Sort to handle duplicates
+
+  function backtrack(path: number[]) {
+    if (path.length === nums.length) {
+      result.push([...path]);
+      return;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      // Skip used numbers
+      if (used[i]) continue;
+
+      // Skip duplicates (only the first occurrence is allowed if the previous one hasn't been used)
+      if (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) continue;
+
+      used[i] = true;
+      path.push(nums[i]);
+
+      backtrack(path);
+
+      path.pop();
+      used[i] = false;
+    }
+  }
+
+  backtrack([]);
+  return result;
+}
