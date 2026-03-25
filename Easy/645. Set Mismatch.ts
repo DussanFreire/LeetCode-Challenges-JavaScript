9120f486557017088492cf0ee3c3290e27e16317
Dussan Freire <dussanfreire@gmail.com>
2026-03-25 11:50:21 -0400

    Create 645. Set Mismatch.ts

---
+++ Easy/645. Set Mismatch.ts
@@ -0,0 +1,36 @@
+// You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+
+// You are given an integer array nums representing the data status of this set after the error.
+// Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+// Example 1:
+// Input: nums = [1,2,2,4]
+// Output: [2,3]
+// Example 2:
+
+// Input: nums = [1,1]
+// Output: [1,2]
+
+// Constraints:
+// 2 <= nums.length <= 104
+// 1 <= nums[i] <= 104
+
+function findErrorNums(nums: number[]): number[] {
+  const n = nums.length;
+  const seenNumbers = new Set<number>();
+  let duplicate = -1;
+  let actualSum = 0;
+
+  for (const num of nums) {
+    if (seenNumbers.has(num)) {
+      duplicate = num;
+    }
+    seenNumbers.add(num);
+    actualSum += num;
+  }
+
+  const expectedSum = (n * (n + 1)) / 2;
+  const missing = expectedSum - (actualSum - duplicate);
+
+  return [duplicate, missing];
+}
