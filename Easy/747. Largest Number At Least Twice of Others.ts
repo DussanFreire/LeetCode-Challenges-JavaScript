276caeedb46ad18543f290eddf6dd30c8faecf2d
Dussan Freire <dussanfreire@gmail.com>
2026-04-15 16:31:37 -0400

    Create 747. Largest Number At Least Twice of Others.ts

---
+++ Easy/747. Largest Number At Least Twice of Others.ts
@@ -0,0 +1,38 @@
+// You are given an integer array nums where the largest integer is unique.
+// Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+
+// Example 1:
+// Input: nums = [3,6,1,0]
+// Output: 1
+// Explanation: 6 is the largest integer.
+// For every other number in the array x, 6 is at least twice as big as x.
+// The index of value 6 is 1, so we return 1.
+
+// Example 2:
+// Input: nums = [1,2,3,4]
+// Output: -1
+// Explanation: 4 is less than twice the value of 3, so we return -1.
+ 
+
+// Constraints:
+// 2 <= nums.length <= 50
+// 0 <= nums[i] <= 100
+// The largest element in nums is unique.
+
+function dominantIndex(nums: number[]): number {
+    let maxVal = -1;
+    let secondMax = -1;
+    let maxIndex = -1;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > maxVal) {
+            secondMax = maxVal;
+            maxVal = nums[i];
+            maxIndex = i;
+        } else if (nums[i] > secondMax) {
+            secondMax = nums[i];
+        }
+    }
+
+    return maxVal >= secondMax * 2 ? maxIndex : -1;
+}
