057ca2e257ea2794e1abf1437cd84326cb76b01f
Dussan Freire <dussanfreire@gmail.com>
2026-07-09 07:52:18 -0400

    Create 1160. Find Words That Can Be Formed by Characters.ts

---
+++ Easy/1160. Find Words That Can Be Formed by Characters.ts
@@ -0,0 +1,62 @@
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an array of strings words and a string chars.
+
+// A string is good if it can be formed by characters from chars (each character can only be used once for each word in words).
+
+// Return the sum of lengths of all good strings in words.
+
+ 
+
+// Example 1:
+
+// Input: words = ["cat","bt","hat","tree"], chars = "atach"
+// Output: 6
+// Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+// Example 2:
+
+// Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+// Output: 10
+// Explanation: The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+ 
+
+// Constraints:
+
+// 1 <= words.length <= 1000
+// 1 <= words[i].length, chars.length <= 100
+// words[i] and chars consist of lowercase English letters.
+
+function countCharacters(words: string[], chars: string): number {
+    const charsCount = new Int32Array(26);
+    for (let i = 0; i < chars.length; i++) {
+        charsCount[chars.charCodeAt(i) - 97]++;
+    }
+
+    let totalLength = 0;
+
+    for (const word of words) {
+        if (canFormWord(word, charsCount)) {
+            totalLength += word.length;
+        }
+    }
+
+    return totalLength;
+}
+
+function canFormWord(word: string, charsCount: Int32Array): boolean {
+    const wordCount = new Int32Array(26);
+    
+    for (let i = 0; i < word.length; i++) {
+        const index = word.charCodeAt(i) - 97;
+        wordCount[index]++;
+        
+        if (wordCount[index] > charsCount[index]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
